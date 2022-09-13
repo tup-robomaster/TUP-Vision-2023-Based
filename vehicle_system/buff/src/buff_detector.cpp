dbd952863305bdffcaf1f440bb9d7a71a9eaac18
Liu Biao <1441488761@qq.com>
2022-09-13 22:38:18 +0800

    Delete vehicle_system directory

--- vehicle_system/buff/src/buff_detector.cpp
+++
@@ -1,7 +0,0 @@
-/*
- * @Description: This is a ros_control learning project!
- * @Author: Liu Biao
- * @Date: 2022-09-05 03:39:07
- * @LastEditTime: 2022-09-05 17:32:28
- * @FilePath: /tup_2023/src/vehicle_system/buff/src/buff_detector.cpp
- */
