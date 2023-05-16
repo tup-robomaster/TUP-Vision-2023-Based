09e4413f9688d98d5aa99114e5757793b9d20452
push-rush <1441488761@qq.com>
2023-05-17 01:37:18 +0800

    debug...

--- WatchDog.sh
+++ WatchDog.sh
@@ -3,37 +3,39 @@
  # @Description: This is a ros-based project!
  # @Author: Liu Biao
  # @Date: 2023-01-02 22:20:13
- # @LastEditTime: 2023-04-16 11:45:18
+ # @LastEditTime: 2023-05-05 15:23:45
  # @FigtPath: /TUP-Vision-2023-Based/WatchDog.sh
 ### 
-name=TUP-Vision-2023-Based
-package=global_user
-launch_file=autoaim_bringup.launch.py
-sec=2 
-cnt=0
+export ROS_HOME="/home/tup/ros2_ws"
+cd /home/tup/ros2_ws
+# 设置需要启动和监视的ros命令
+declare -a commands=("ros2 launch global_user autoaim_bringup.launch.py")
 
-cd /home/tup/Desktop/$name/
-source /opt/ros/galactic/setup.bash
-colcon build --symlink-install
-source install/setup.bash
-ros2 launch $package $launch_file
+# 定义函数来启动ros命令
+start_commands() {
+  for cmd in "${commands[@]}"
+  do
+	gnome-terminal -- bash -c "source /opt/openvino/setupvars.sh;source /opt/ros/galactic/setup.bash;cd $(pwd);source install/setup.bash;$cmd;exec bash;"
+  done
+}
 
-while true
-do 
-    count=`ps -ef | grep $launch_file | grep -v "grep" | wc -1`
+# 定义函数来监视ros进程是否在运行
+watch_commands() {
+  while true
+  do
+    for cmd in "${commands[@]}"
+    do
+      process_name=$(echo $cmd | awk '{print $1}') # 提取进程名
+      if ! pgrep -f "$cmd" > /dev/null
+      then
+        echo "$cmd 未在运行, 重新启动"
+	gnome-terminal -- bash -c "source /opt/openvino/setupvars.sh;source /opt/ros/galactic/setup.bash;cd $(pwd);source install/setup.bash;$cmd;exec bash;"
+      fi
+    done
+    sleep 1 # 设置睡眠时间，以便在下一次检查之前给进程足够的时间运行
+  done
+}
 
-    if [ $count -gt 1]; then
-        echo "The $launch_file is alive!"
-        sleep $sec
-    else
-        kill $launch_file
-        ros2 launch $package $launch_file
-        cnt=cnt+1
-        sleep $sec
-        if [ $cnt -gt 9]; then
-            # echo "Reboot!"
-            reboot
-        fi
-    fi
-done
-
+# 启动ros命令和监视进程
+start_commands
+watch_commands
